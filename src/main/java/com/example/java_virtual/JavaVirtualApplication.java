960fca56ec4a3c9ce8c0e896fa5208bfa2d7d700
Joao Gomes <joaog@kmslh.com>
2025-11-18 14:19:30 +0000

    Initial commit

---
+++ src/main/java/com/example/java_virtual/JavaVirtualApplication.java
@@ -0,0 +1,13 @@
+package com.example.java_virtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaVirtualApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/java_virtual/JavaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.java_virtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
